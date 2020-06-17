3734fc3f03b2292f780a6e76e8a393ab3a8155fa
André Biseth <andre.biseth@onyxcentersource.com>
2020-06-17 08:54:13 +0200

    added libtool

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ export PATH="/usr/share/dotnet":"$PATH"
 # Install AIS Relay
 dotnet publish --output /usr/share/aisrelay
 
-apt install -y git build-essential autotools-dev autoconf libusb-dev
+apt install -y git build-essential autotools-dev autoconf libtool libusb-dev
 
 # Clone and install RTL-SDR
 git clone git://git.osmocom.org/rtl-sdr.git
