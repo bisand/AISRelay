8d504f2a6898a27f4b1fce57522f479c8ac54502
André Biseth <andre@biseth.net>
2020-07-02 13:11:45 +0200

    Added arguments to install script

--- install.sh
+++ install.sh
@@ -1,53 +1,107 @@
 #!/bin/bash
 
+remove_whitespace() {
+    local var="$1"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo "$var"
+}
+
 echo "Starting AIS installation..."
+
+listen_port=11010
+broadcast_port=2947
+publish_endpoints=""
+tuner_gain=49.60
+ppm_error=197
+do_install="true"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -l | --listen-port)
+        listen_port="$2"
+        ;;
+    -b | --broadcast-port)
+        broadcast_port="$2"
+        ;;
+    -e | --publish-endpoints)
+        publish_endpoints="$publish_endpoints $2"
+        ;;
+    -g | --tuner-gain)
+        tuner_gain="$2"
+        ;;
+    -p | --ppm-error)
+        ppm_error="$2"
+        ;;
+    -c | --config-only)
+        do_install="false"
+        ;;
+    esac
+    shift
+done
+
+listen_port=$(remove_whitespace "$listen_port")
+broadcast_port=$(remove_whitespace "$broadcast_port")
+publish_endpoints=$(remove_whitespace "$publish_endpoints")
+tuner_gain=$(remove_whitespace "$tuner_gain")
+ppm_error=$(remove_whitespace "$ppm_error")
+
+echo "Listen Port: ${listen_port}"
+echo "Broadcast Port: ${broadcast_port}"
+echo "Publish Endpoints: ${publish_endpoints}"
+echo "Tuner gain: ${tuner_gain}"
+echo "PPM error: ${ppm_error}"
 
 # Switch to tmp directory
 rm -Rf /tmp/aisrelay
 cp -R . /tmp/aisrelay
 cd /tmp/aisrelay
 
-# Install prerequisites.
-echo "Installing prerequisites..."
-apt install -y git cmake build-essential libusb-1.0-0-dev libboost-system-dev libboost-program-options-dev &>> /tmp/aisrelay.log
+if [[ "${do_install}" == "true" ]]; then
+    # Install prerequisites.
+    echo "Installing prerequisites..."
+    apt install -y git cmake build-essential libusb-1.0-0-dev libboost-system-dev libboost-program-options-dev &>>/tmp/aisrelay.log
 
-# Clone and install RTL-SDR
-echo "Building and installing RTL-SRD. This could take several minutes..."
-git clone git://git.osmocom.org/rtl-sdr.git &>> /tmp/aisrelay.log
-cd rtl-sdr/
-mkdir build
-cd build
-cmake ../ -DINSTALL_UDEV_RULES=ON &>> /tmp/aisrelay.log
-make &>> /tmp/aisrelay.log
-sudo make install &>> /tmp/aisrelay.log
-sudo ldconfig &>> /tmp/aisrelay.log
-cd ../..
-echo "Done."
+    # Clone and install RTL-SDR
+    echo "Building and installing RTL-SRD. This could take several minutes..."
+    git clone git://git.osmocom.org/rtl-sdr.git &>>/tmp/aisrelay.log
+    cd rtl-sdr/
+    mkdir build
+    cd build
+    cmake ../ -DINSTALL_UDEV_RULES=ON &>>/tmp/aisrelay.log
+    make &>>/tmp/aisrelay.log
+    sudo make install &>>/tmp/aisrelay.log
+    sudo ldconfig &>>/tmp/aisrelay.log
+    cd ../..
+    echo "Done."
 
-echo "Building and installing RTL-AIS. This could take several minutes..."
-# Adding include and lib path to prevent build errors on rtl-ais
-sed -i.bak 's/^\(prefix=\).*/\1\/usr/' /usr/local/lib/pkgconfig/librtlsdr.pc
-sed -i.bak 's/^\(libdir=\).*/\1${prefix}\/lib/' /usr/local/lib/pkgconfig/librtlsdr.pc
-sed -i.bak 's/^\(includedir=\).*/\1${prefix}\/include/' /usr/local/lib/pkgconfig/librtlsdr.pc
+    echo "Building and installing RTL-AIS. This could take several minutes..."
+    # Adding include and lib path to prevent build errors on rtl-ais
+    sed -i.bak 's/^\(prefix=\).*/\1\/usr/' /usr/local/lib/pkgconfig/librtlsdr.pc
+    sed -i.bak 's/^\(libdir=\).*/\1${prefix}\/lib/' /usr/local/lib/pkgconfig/librtlsdr.pc
+    sed -i.bak 's/^\(includedir=\).*/\1${prefix}\/include/' /usr/local/lib/pkgconfig/librtlsdr.pc
 
-# Clone and install RTL-AIS
-git clone https://github.com/dgiardini/rtl-ais &>> /tmp/aisrelay.log
-cd rtl-ais
-make &>> /tmp/aisrelay.log
-make install &>> /tmp/aisrelay.log
-cd ..
-echo "Done."
+    # Clone and install RTL-AIS
+    git clone https://github.com/dgiardini/rtl-ais &>>/tmp/aisrelay.log
+    cd rtl-ais
+    make &>>/tmp/aisrelay.log
+    make install &>>/tmp/aisrelay.log
+    cd ..
+    echo "Done."
 
-echo "Building and installing AISRelay. This may take up to 30 minutes on slow systems..."
-cd src
-make &>> /tmp/aisrelay.log
-make install &>> /tmp/aisrelay.log
-cd ..
-echo "Done."
+    echo "Building and installing AISRelay. This may take up to 30 minutes on slow systems..."
+    cd src
+    make &>>/tmp/aisrelay.log
+    make install &>>/tmp/aisrelay.log
+    cd ..
+    echo "Done."
+fi
 
 echo "Creating and starting services..."
 # Create rtl_ais service
-cat > /etc/systemd/system/rtl_ais.service << EOF
+cat >/etc/systemd/system/rtl_ais.service <<EOF
 [Unit]
 Description=rtl_ais, A simple AIS tuner and generic dual-frequency FM demodulator
 After=network.target
@@ -55,7 +109,7 @@ StartLimitIntervalSec=0
 
 [Service]
 WorkingDirectory=/tmp
-ExecStart=/usr/local/bin/rtl_ais -p197 -g49.60
+ExecStart=/usr/local/bin/rtl_ais -p${ppm_error} -g${tuner_gain}
 Restart=always
 # Restart service after 5 seconds if the dotnet service crashes:
 RestartSec=5
@@ -68,7 +122,7 @@ WantedBy=multi-user.target
 EOF
 
 # Create AIS Relay service
-cat > /etc/systemd/system/aisrelay.service << EOF
+cat >/etc/systemd/system/aisrelay.service <<EOF
 [Unit]
 Description=AIS Relay for broadcasting AIS messages to local network and multiple external UDP and TCP endpoints like MarineTraffic.
 After=network.target
@@ -76,7 +130,7 @@ StartLimitIntervalSec=0
 
 [Service]
 WorkingDirectory=/tmp
-ExecStart=/usr/local/bin/aisrelay --listen 10110 --broadcast 2948 --publish-endpoints udp://5.9.207.224:8317
+ExecStart=/usr/local/bin/aisrelay --listen ${listen_port} --broadcast ${broadcast_port} --publish-endpoints ${publish_endpoints}
 Restart=always
 # Restart service after 10 seconds if the dotnet service crashes:
 RestartSec=10
@@ -91,15 +145,15 @@ WantedBy=multi-user.target
 EOF
 
 # Enable and start services.
-systemctl enable rtl_ais &>> /tmp/aisrelay.log
-systemctl enable aisrelay &>> /tmp/aisrelay.log
-systemctl start rtl_ais &>> /tmp/aisrelay.log
-systemctl start aisrelay &>> /tmp/aisrelay.log
+systemctl enable rtl_ais &>>/tmp/aisrelay.log
+systemctl enable aisrelay &>>/tmp/aisrelay.log
+systemctl start rtl_ais &>>/tmp/aisrelay.log
+systemctl start aisrelay &>>/tmp/aisrelay.log
 echo "Done."
 
 # Cleaning up
 echo "Cleaning up..."
-rm -Rf /tmp/aisrelay &>> /tmp/aisrelay.log
+rm -Rf /tmp/aisrelay &>>/tmp/aisrelay.log
 echo "Done."
 echo "Installation complete!"
 echo "See log file for more info (/tmp/aisrelay.log)"
